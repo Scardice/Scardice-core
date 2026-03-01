Szzrain/sealdice-core

fix(log): 修复日志系统中不存在的日志，新增时会误创建ID=0假记录的问题

migrate/v2/enter.go
@@ -9,6 +9,7 @@ import (
 	v144 "sealdice-core/migrate/v2/v144"
 	v150 "sealdice-core/migrate/v2/v150"
 	v151 "sealdice-core/migrate/v2/v151"
+	v160 "sealdice-core/migrate/v2/v160"
 	operator "sealdice-core/utils/dboperator/engine"
 	upgrade "sealdice-core/utils/upgrader"
 	"sealdice-core/utils/upgrader/store"
@@ -31,6 +32,8 @@ func InitUpgrader(operator operator.DatabaseOperator) error {
 	mgr.Register(v150.V150FixGroupInfoMigration)
 	// v151注册
 	mgr.Register(v151.V151GORMCleanMigration)
+	// v160注册
+	mgr.Register(v160.V160LogIDZeroCleanMigration)
 	err := mgr.ApplyAll()
 	if err != nil {
 		return err

migrate/v2/v160/v160_logid0.go
@@ -1,0 +1,67 @@
+package v160
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+
+	"sealdice-core/model"
+	"sealdice-core/utils/constant"
+	operator "sealdice-core/utils/dboperator/engine"
+	upgrade "sealdice-core/utils/upgrader"
+)
+
+// V160LogIDZeroCleanMigrate 清理 log_id = 0 的日志行与日志表残留
+func V160LogIDZeroCleanMigrate(dboperator operator.DatabaseOperator, logf func(string)) error {
+	db := dboperator.GetLogDB(constant.WRITE)
+
+	itemResult := db.Where("log_id = 0").Delete(&model.LogOneItem{})
+	if itemResult.Error != nil {
+		return itemResult.Error
+	}
+
+	logResult := db.Where("id = 0").Delete(&model.LogInfo{})
+	if logResult.Error != nil {
+		return logResult.Error
+	}
+
+	// log_id=0 清理后，回填 logs.size，避免调用方读取到过期计数
+	recountResult := db.Model(&model.LogInfo{}).Update("size", gorm.Expr(
+		"(SELECT COUNT(1) FROM log_items WHERE log_items.log_id = logs.id AND log_items.removed IS NULL)",
+	))
+	if recountResult.Error != nil {
+		return recountResult.Error
+	}
+
+	if itemResult.RowsAffected > 0 {
+		logf(fmt.Sprintf("数据修复 - LogItems表，删除了 %d 条记录", itemResult.RowsAffected))
+	} else {
+		logf("数据修复 - LogItems表，没有需要删除的记录")
+	}
+
+	if logResult.RowsAffected > 0 {
+		logf(fmt.Sprintf("数据修复 - Logs表，删除了 %d 条记录", logResult.RowsAffected))
+	} else {
+		logf("数据修复 - Logs表，没有需要删除的记录")
+	}
+	logf(fmt.Sprintf("数据修复 - Logs表，重算了 %d 条size字段", recountResult.RowsAffected))
+
+	return nil
+}
+
+var V160LogIDZeroCleanMigration = upgrade.Upgrade{
+	ID: "008_V160LogIDZeroCleanMigration",
+	Description: `
+# 升级说明
+清理 log_id = 0 的错误日志数据
+`,
+	Apply: func(logf func(string), dbOperator operator.DatabaseOperator) error {
+		logf("[INFO] V160清理log_id=0数据开始")
+		err := V160LogIDZeroCleanMigrate(dbOperator, logf)
+		if err != nil {
+			return err
+		}
+		logf("[INFO] V160清理log_id=0数据处置完毕")
+		return nil
+	},
+}

Scardice/Scardice-core

migrate/v2/enter.go
@@ -9,6 +9,7 @@ import (
 	v144 "Scardice-core/migrate/v2/v144"
 	v150 "Scardice-core/migrate/v2/v150"
 	v151 "Scardice-core/migrate/v2/v151"
+	v160 "Scardice-core/migrate/v2/v160"
 	operator "Scardice-core/utils/dboperator/engine"
 	upgrade "Scardice-core/utils/upgrader"
 	"Scardice-core/utils/upgrader/store"
@@ -31,6 +32,8 @@ func InitUpgrader(operator operator.DatabaseOperator) error {
 	mgr.Register(v150.V150FixGroupInfoMigration)
 	// v151注册
 	mgr.Register(v151.V151GORMCleanMigration)
+	// v160注册
+	mgr.Register(v160.V160LogIDZeroCleanMigration)
 	err := mgr.ApplyAll()
 	if err != nil {
 		return err

migrate/v2/v160/v160_logid0.go
@@ -1,0 +1,67 @@
+package v160
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+
+	"Scardice-core/model"
+	"Scardice-core/utils/constant"
+	operator "Scardice-core/utils/dboperator/engine"
+	upgrade "Scardice-core/utils/upgrader"
+)
+
+// V160LogIDZeroCleanMigrate 清理 log_id = 0 的日志行与日志表残留
+func V160LogIDZeroCleanMigrate(dboperator operator.DatabaseOperator, logf func(string)) error {
+	db := dboperator.GetLogDB(constant.WRITE)
+
+	itemResult := db.Where("log_id = 0").Delete(&model.LogOneItem{})
+	if itemResult.Error != nil {
+		return itemResult.Error
+	}
+
+	logResult := db.Where("id = 0").Delete(&model.LogInfo{})
+	if logResult.Error != nil {
+		return logResult.Error
+	}
+
+	// log_id=0 清理后，回填 logs.size，避免调用方读取到过期计数
+	recountResult := db.Model(&model.LogInfo{}).Update("size", gorm.Expr(
+		"(SELECT COUNT(1) FROM log_items WHERE log_items.log_id = logs.id AND log_items.removed IS NULL)",
+	))
+	if recountResult.Error != nil {
+		return recountResult.Error
+	}
+
+	if itemResult.RowsAffected > 0 {
+		logf(fmt.Sprintf("数据修复 - LogItems表，删除了 %d 条记录", itemResult.RowsAffected))
+	} else {
+		logf("数据修复 - LogItems表，没有需要删除的记录")
+	}
+
+	if logResult.RowsAffected > 0 {
+		logf(fmt.Sprintf("数据修复 - Logs表，删除了 %d 条记录", logResult.RowsAffected))
+	} else {
+		logf("数据修复 - Logs表，没有需要删除的记录")
+	}
+	logf(fmt.Sprintf("数据修复 - Logs表，重算了 %d 条size字段", recountResult.RowsAffected))
+
+	return nil
+}
+
+var V160LogIDZeroCleanMigration = upgrade.Upgrade{
+	ID: "008_V160LogIDZeroCleanMigration",
+	Description: `
+# 升级说明
+清理 log_id = 0 的错误日志数据
+`,
+	Apply: func(logf func(string), dbOperator operator.DatabaseOperator) error {
+		logf("[INFO] V160清理log_id=0数据开始")
+		err := V160LogIDZeroCleanMigrate(dbOperator, logf)
+		if err != nil {
+			return err
+		}
+		logf("[INFO] V160清理log_id=0数据处置完毕")
+		return nil
+	},
+}