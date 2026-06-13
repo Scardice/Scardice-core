cad692525242cc1cc66883deda980ddf6f223b38
lyjjl <104555884+lyjjl@users.noreply.github.com>
2026-06-14 04:49:05 +0800

    fix: 使用 all: 前缀将下划线开头的静态文件打入 embed.FS 修复白屏

--- static/static.go
+++ static/static.go
@@ -7,7 +7,9 @@ import (
 // //go:generate go run gen/download-fe.go
 // NOTE(lyjjl): download-fe.go 负责下载 UI 资源，但是目前这一块逻辑还没有完全从海豹中剥离出来，暂时禁用
 
-//go:embed frontend
+// NOTE(lyjjl): 前端构建产物文件名可能会以“_”开头，如果不写“all:”这些文件会被忽略 https://pkg.go.dev/embed
+//
+//go:embed all:frontend
 var Frontend embed.FS
 
 //go:embed scripts
