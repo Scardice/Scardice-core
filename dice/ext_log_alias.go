sealdice/sealdice-core

fix(log): 调整上传文件命名并增加哈希取日志方案 (#1701)
我将行使特权……

dice/ext_log_alias.go
@@ -1,0 +1,149 @@
+package dice
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"github.com/cespare/xxhash/v2"
+
+	"sealdice-core/dice/service"
+	engine "sealdice-core/utils/dboperator/engine"
+)
+
+const minLogAliasKeyLength = 8
+
+type logNameAliasEntry struct {
+	Name     string
+	FullHash string
+	Key      string
+}
+
+type logNameAliasIndex struct {
+	entries []logNameAliasEntry
+	byKey   map[string]string
+	byName  map[string]string
+}
+
+func buildLogNameAliasIndex(operator engine.DatabaseOperator, groupID string) (*logNameAliasIndex, error) {
+	names, err := service.LogGetList(operator, groupID)
+	if err != nil {
+		return nil, err
+	}
+
+	entries, err := buildLogNameAliasEntries(names, minLogAliasKeyLength)
+	if err != nil {
+		return nil, err
+	}
+
+	index := &logNameAliasIndex{
+		entries: entries,
+		byKey:   map[string]string{},
+		byName:  map[string]string{},
+	}
+	for _, entry := range entries {
+		index.byKey[entry.Key] = entry.Name
+		index.byName[entry.Name] = entry.Name
+	}
+	return index, nil
+}
+
+func buildLogNameAliasEntries(names []string, minPrefixLen int) ([]logNameAliasEntry, error) {
+	if minPrefixLen < 1 {
+		minPrefixLen = 1
+	}
+
+	entries := make([]logNameAliasEntry, len(names))
+	keyLens := make([]int, len(names))
+	for i, name := range names {
+		fullHash := logNameHashHex(name)
+		if minPrefixLen > len(fullHash) {
+			keyLens[i] = len(fullHash)
+		} else {
+			keyLens[i] = minPrefixLen
+		}
+		entries[i] = logNameAliasEntry{
+			Name:     name,
+			FullHash: fullHash,
+		}
+	}
+
+	for {
+		counts := map[string]int{}
+		for i, entry := range entries {
+			counts[entry.FullHash[:keyLens[i]]]++
+		}
+
+		changed := false
+		for i, entry := range entries {
+			key := entry.FullHash[:keyLens[i]]
+			if counts[key] <= 1 {
+				continue
+			}
+			if keyLens[i] >= len(entry.FullHash) {
+				return nil, errors.New("日志 hash 键冲突，请改用原始名称")
+			}
+			keyLens[i]++
+			changed = true
+		}
+
+		if !changed {
+			break
+		}
+	}
+
+	for i, entry := range entries {
+		entry.Key = entry.FullHash[:keyLens[i]]
+		entries[i] = entry
+	}
+	return entries, nil
+}
+
+func logNameHashHex(name string) string {
+	return fmt.Sprintf("%016x", xxhash.Sum64String(name))
+}
+
+func (index *logNameAliasIndex) Resolve(input string) (string, bool) {
+	if input == "" {
+		return "", false
+	}
+	if name, exists := index.byName[input]; exists {
+		return name, true
+	}
+	if name, exists := index.byKey[strings.ToLower(input)]; exists {
+		return name, true
+	}
+	return "", false
+}
+
+func resolveLogNameForGroup(operator engine.DatabaseOperator, groupID, input string) (string, error) {
+	if input == "" {
+		return "", nil
+	}
+	index, err := buildLogNameAliasIndex(operator, groupID)
+	if err != nil {
+		return "", err
+	}
+	if resolved, ok := index.Resolve(input); ok {
+		return resolved, nil
+	}
+	return input, nil
+}
+
+func formatLogNameForDisplay(name string) string {
+	if strings.TrimSpace(name) != name {
+		return strconv.Quote(name)
+	}
+	for _, r := range name {
+		if unicode.IsControl(r) {
+			return strconv.Quote(name)
+		}
+	}
+	return name
+}
+
+func formatLogNameListLine(entry logNameAliasEntry) string {
+	return fmt.Sprintf("- 【%s】: %s", entry.Key, formatLogNameForDisplay(entry.Name))
+}

dice/ext_log_alias_test.go
@@ -1,0 +1,221 @@
+//nolint:testpackage
+package dice
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"testing"
+
+	sealdiceLogger "sealdice-core/logger"
+	"sealdice-core/model"
+	"sealdice-core/utils/constant"
+)
+
+func newLogAliasTestDB(t *testing.T) *mockDatabaseOperator {
+	t.Helper()
+
+	mockDB, err := newMockDatabaseOperator(filepath.Join(t.TempDir(), "logs.db"))
+	if err != nil {
+		t.Fatalf("newMockDatabaseOperator: %v", err)
+	}
+	t.Cleanup(mockDB.Close)
+
+	db := mockDB.GetLogDB(constant.WRITE)
+	if err := db.AutoMigrate(&model.LogInfo{}, &model.LogOneItem{}); err != nil {
+		t.Fatalf("AutoMigrate: %v", err)
+	}
+	return mockDB
+}
+
+func appendTestLog(t *testing.T, mockDB *mockDatabaseOperator, groupID, logName string) {
+	t.Helper()
+
+	ok := LogAppend(&MsgContext{Dice: &Dice{DBOperator: mockDB}}, groupID, logName, &model.LogOneItem{
+		Nickname: "tester",
+		IMUserID: "user",
+		Message:  "line",
+	})
+	if !ok {
+		t.Fatalf("LogAppend failed for %q", logName)
+	}
+}
+
+func TestBuildLogNameAliasIndexUsesWrappedKeysAndResolvesInput(t *testing.T) {
+	mockDB := newLogAliasTestDB(t)
+	groupID := "QQ-Group:1001"
+
+	appendTestLog(t, mockDB, groupID, "normal-log")
+	appendTestLog(t, mockDB, groupID, " spaced\nname ")
+
+	index, err := buildLogNameAliasIndex(mockDB, groupID)
+	if err != nil {
+		t.Fatalf("buildLogNameAliasIndex: %v", err)
+	}
+	if len(index.entries) != 2 {
+		t.Fatalf("len(entries) = %d, want 2", len(index.entries))
+	}
+
+	for _, entry := range index.entries {
+		if len(entry.Key) < 8 {
+			t.Fatalf("key %q shorter than 8 chars", entry.Key)
+		}
+		if entry.Name == "normal-log" {
+			if got, ok := index.Resolve(entry.Key); !ok || got != entry.Name {
+				t.Fatalf("Resolve(%q) = (%q, %v), want (%q, true)", entry.Key, got, ok, entry.Name)
+			}
+		}
+	}
+
+	if got := formatLogNameForDisplay(" spaced\nname "); got != strconv.Quote(" spaced\nname ") {
+		t.Fatalf("formatLogNameForDisplay() = %q", got)
+	}
+	if got := formatLogNameListLine(logNameAliasEntry{Key: "deadbeef", Name: "normal-log"}); got != "- 【deadbeef】: normal-log" {
+		t.Fatalf("formatLogNameListLine() = %q", got)
+	}
+}
+
+func TestBuildLogNameAliasEntriesExtendsConflictingPrefixes(t *testing.T) {
+	buckets := map[byte][]string{}
+	for i := range 4096 {
+		name := fmt.Sprintf("log-%d", i)
+		hash := logNameHashHex(name)
+		prefix := hash[0]
+		buckets[prefix] = append(buckets[prefix], name)
+		if len(buckets[prefix]) >= 2 {
+			break
+		}
+	}
+
+	var names []string
+	for _, candidates := range buckets {
+		if len(candidates) >= 2 {
+			names = candidates[:2]
+			break
+		}
+	}
+	if len(names) != 2 {
+		t.Fatal("failed to generate colliding 1-char hash prefixes")
+	}
+
+	entries, err := buildLogNameAliasEntries(names, 1)
+	if err != nil {
+		t.Fatalf("buildLogNameAliasEntries: %v", err)
+	}
+
+	keys := map[string]struct{}{}
+	grew := false
+	for _, entry := range entries {
+		if _, exists := keys[entry.Key]; exists {
+			t.Fatalf("duplicate key %q", entry.Key)
+		}
+		keys[entry.Key] = struct{}{}
+		if len(entry.Key) > 1 {
+			grew = true
+		}
+	}
+	if !grew {
+		t.Fatal("expected at least one conflicting prefix to grow beyond min length")
+	}
+}
+
+func TestResolveLogNameForGroupPrefersExactNameBeforeAlias(t *testing.T) {
+	mockDB := newLogAliasTestDB(t)
+	groupID := "QQ-Group:1002"
+
+	appendTestLog(t, mockDB, groupID, "abcdef12")
+	appendTestLog(t, mockDB, groupID, "other-log")
+
+	index, err := buildLogNameAliasIndex(mockDB, groupID)
+	if err != nil {
+		t.Fatalf("buildLogNameAliasIndex: %v", err)
+	}
+
+	var alias string
+	for _, entry := range index.entries {
+		if entry.Name == "other-log" {
+			alias = entry.Key
+			break
+		}
+	}
+	if alias == "" {
+		t.Fatal("expected alias for other-log")
+	}
+	if strings.EqualFold(alias, "abcdef12") {
+		t.Fatal("test setup produced ambiguous exact-name collision")
+	}
+
+	got, err := resolveLogNameForGroup(mockDB, groupID, "abcdef12")
+	if err != nil {
+		t.Fatalf("resolveLogNameForGroup exact: %v", err)
+	}
+	if got != "abcdef12" {
+		t.Fatalf("resolve exact = %q, want %q", got, "abcdef12")
+	}
+}
+
+func TestLogSendToBackendAcceptsAliasKey(t *testing.T) {
+	mockDB := newLogAliasTestDB(t)
+	groupID := "QQ-Group:1003"
+	logName := "special name"
+	appendTestLog(t, mockDB, groupID, logName)
+
+	index, err := buildLogNameAliasIndex(mockDB, groupID)
+	if err != nil {
+		t.Fatalf("buildLogNameAliasIndex: %v", err)
+	}
+	var alias string
+	for _, entry := range index.entries {
+		if entry.Name == logName {
+			alias = entry.Key
+			break
+		}
+	}
+	if alias == "" {
+		t.Fatal("expected alias for special name")
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/dice/api/log" {
+			t.Fatalf("unexpected path %q", r.URL.Path)
+		}
+		if parseErr := r.ParseMultipartForm(1 << 20); parseErr != nil {
+			t.Fatalf("ParseMultipartForm: %v", parseErr)
+		}
+		if got := r.FormValue("name"); got != logName {
+			t.Fatalf("uploaded name = %q, want %q", got, logName)
+		}
+		_, _ = w.Write([]byte(`{"url":"https://example.com/log"}`))
+	}))
+	defer server.Close()
+
+	oldBackends := BackendUrls
+	BackendUrls = []string{server.URL}
+	t.Cleanup(func() { BackendUrls = oldBackends })
+
+	ctx := &MsgContext{
+		Dice: &Dice{
+			BaseConfig: BaseConfig{DataDir: t.TempDir()},
+			DBOperator: mockDB,
+			Logger:     sealdiceLogger.M(),
+		},
+		EndPoint: &EndPointInfo{EndPointInfoBase: EndPointInfoBase{UserID: "UI:1000"}},
+	}
+
+	unofficial, url, notice, err := LogSendToBackend(ctx, groupID, alias)
+	if err != nil {
+		t.Fatalf("LogSendToBackend: %v", err)
+	}
+	if unofficial {
+		t.Fatal("expected official backend path")
+	}
+	if notice != "" {
+		t.Fatalf("unexpected notice %q", notice)
+	}
+	if url != "https://example.com/log" {
+		t.Fatalf("url = %q", url)
+	}
+}

dice/storylog/upload_v105.go
@@ -21,7 +21,6 @@ import (
 
 	"sealdice-core/dice/service"
 	"sealdice-core/model"
-	"sealdice-core/utils"
 )
 
 func GetLogTxtAndParquetFile(env UploadEnv) (*os.File, *bytes.Buffer, error) {
@@ -34,10 +33,8 @@ func GetLogTxtAndParquetFile(env UploadEnv) (*os.File, *bytes.Buffer, error) {
 			),
 		))
 	buf := new(bytes.Buffer)
-	tempLog, err := os.CreateTemp("", fmt.Sprintf(
-		"%s(*).txt",
-		utils.FilenameClean("sealdice_v105_prefix_"),
-	))
+	tempPattern, _ := buildTempPattern("sealdice_v105_prefix")
+	tempLog, err := os.CreateTemp("", tempPattern)
 	if err != nil {
 		return nil, nil, errors.New("log导出出现未知错误")
 	}
@@ -76,12 +73,16 @@ func GetLogTxtAndParquetFile(env UploadEnv) (*os.File, *bytes.Buffer, error) {
 				for _, line := range cursorLines {
 					timeTxt := time.Unix(line.Time, 0).Format("2006-01-02 15:04:05")
 					text := fmt.Sprintf("%s(%v) %s\n%s\n\n", line.Nickname, line.IMUserID, timeTxt, line.Message)
-					_, _ = tempLog.WriteString(text)
+					if _, err0 = tempLog.WriteString(text); err0 != nil {
+						resultCh <- fmt.Errorf("写入日志导出临时文件失败: %w", err0)
+						return
+					}
 					counter++
 				}
 				// ========== 新增：每批写入后强制同步 ==========
 				if err = tempLog.Sync(); err != nil { // 确保批次数据落盘
 					resultCh <- fmt.Errorf("批次同步失败: %w", err)
+					return
 				}
 
 				_, err0 = parquetBuffer.Write(cursorLines)
@@ -132,7 +133,7 @@ func GetLogTxtAndParquetFile(env UploadEnv) (*os.File, *bytes.Buffer, error) {
 }
 
 // 一个不那么激进的改版
-func uploadV105(env UploadEnv) (string, error) {
+func uploadV105(env UploadEnv) (string, string, error) {
 	_ = os.MkdirAll(env.Dir, 0o755)
 
 	url, uploadTS, updateTS, _ := service.LogGetUploadInfo(env.Db, env.GroupID, env.LogName)
@@ -145,7 +146,7 @@ func uploadV105(env UploadEnv) (string, error) {
 			time.Unix(updateTS, 0).Format("2006-01-02 15:04:05"),
 			url,
 		)
-		return url, nil
+		return url, env.Notice, nil
 	}
 	if len(url) == 0 {
 		env.Log.Infof("没有查询到之前上传的URL Log:%s.%s", env.GroupID, env.LogName)
@@ -160,11 +161,15 @@ func uploadV105(env UploadEnv) (string, error) {
 
 	file, b, err := GetLogTxtAndParquetFile(env)
 	if err != nil {
-		return "", err
+		return "", env.Notice, err
 	}
+	defer func() {
+		_ = file.Close()
+		_ = os.Remove(file.Name())
+	}()
 	err = formatAndBackupV105(&env, file, b)
 	if err != nil {
-		return "", err
+		return "", env.Notice, err
 	}
 
 	url = parquetUploadToSealBackends(env, b)
@@ -172,31 +177,48 @@ func uploadV105(env UploadEnv) (string, error) {
 		env.Log.Errorf("记录Log上传信息失败: %v", errDB)
 	}
 	if len(url) == 0 {
-		return "", errors.New("上传 log 到服务器失败，未能获取染色器链接")
+		return "", env.Notice, errors.New("上传 log 到服务器失败，未能获取染色器链接")
 	}
-	return url, nil
+	return url, env.Notice, nil
 }
 
 // formatAndBackup 将导出的日志序列化到 env.data 并存储为本地 zip
-func formatAndBackupV105(env *UploadEnv, tempLog *os.File, parquetFile *bytes.Buffer) error {
-	fzip, _ := os.OpenFile(
-		filepath.Join(env.Dir, utils.FilenameClean(fmt.Sprintf(
-			"%s_%s.%s.zip",
-			env.GroupID, env.LogName, time.Now().Format("060102150405"),
-		))),
+func formatAndBackupV105(env *UploadEnv, tempLog *os.File, parquetFile *bytes.Buffer) (err error) {
+	filename, notice := buildLogBackupFilename(env.GroupID, env.LogName, time.Now())
+	env.appendNotice(notice)
+	fzip, err := os.OpenFile(
+		filepath.Join(env.Dir, filename),
 		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
 		0o600,
 	)
+	if err != nil {
+		return fmt.Errorf("创建日志备份文件失败: %w", err)
+	}
 	writer := zip.NewWriter(fzip)
-	var err error
+	defer func() {
+		if closeErr := writer.Close(); err == nil && closeErr != nil {
+			err = fmt.Errorf("关闭日志压缩文件失败: %w", closeErr)
+		}
+		if closeErr := fzip.Close(); err == nil && closeErr != nil {
+			err = fmt.Errorf("关闭日志备份文件失败: %w", closeErr)
+		}
+	}()
 	// 写入README文件
 	{
-		wr, _ := writer.Create(ExportReadmeFilename)
-		_, _ = wr.Write([]byte(ExportReadmeContent))
+		readmeWriter, createErr := writer.Create(ExportReadmeFilename)
+		if createErr != nil {
+			return fmt.Errorf("创建README失败: %w", createErr)
+		}
+		if _, writeErr := readmeWriter.Write([]byte(ExportReadmeContent)); writeErr != nil {
+			return fmt.Errorf("写入README失败: %w", writeErr)
+		}
 	}
 	// 写入文本TXT文件
 
-	fileWriter, _ := writer.Create(ExportTxtFilename)
+	fileWriter, err := writer.Create(ExportTxtFilename)
+	if err != nil {
+		return fmt.Errorf("创建文本日志文件失败: %w", err)
+	}
 	if _, err = io.Copy(fileWriter, tempLog); err != nil {
 		return fmt.Errorf("写入文本日志文件失败: %w", err)
 	}
@@ -209,11 +231,7 @@ func formatAndBackupV105(env *UploadEnv, tempLog *os.File, parquetFile *bytes.Bu
 	if _, err = parquetWriter.Write(parquetFile.Bytes()); err != nil {
 		return fmt.Errorf("写入Parquet文件失败: %w", err)
 	}
-
-	_ = writer.Close()
-	_ = fzip.Close()
-
-	return err
+	return nil
 }
 
 func parquetUploadToSealBackends(env UploadEnv, data io.Reader) string {

Scardice/Scardice-core

(cherry picked from commit 8044f2f8cc36e3203b01eaa2b010dd1d1b718d59)

dice/ext_log_alias.go
@@ -1,0 +1,149 @@
+package dice
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"github.com/cespare/xxhash/v2"
+
+	"Scardice-core/dice/service"
+	engine "Scardice-core/utils/dboperator/engine"
+)
+
+const minLogAliasKeyLength = 8
+
+type logNameAliasEntry struct {
+	Name     string
+	FullHash string
+	Key      string
+}
+
+type logNameAliasIndex struct {
+	entries []logNameAliasEntry
+	byKey   map[string]string
+	byName  map[string]string
+}
+
+func buildLogNameAliasIndex(operator engine.DatabaseOperator, groupID string) (*logNameAliasIndex, error) {
+	names, err := service.LogGetList(operator, groupID)
+	if err != nil {
+		return nil, err
+	}
+
+	entries, err := buildLogNameAliasEntries(names, minLogAliasKeyLength)
+	if err != nil {
+		return nil, err
+	}
+
+	index := &logNameAliasIndex{
+		entries: entries,
+		byKey:   map[string]string{},
+		byName:  map[string]string{},
+	}
+	for _, entry := range entries {
+		index.byKey[entry.Key] = entry.Name
+		index.byName[entry.Name] = entry.Name
+	}
+	return index, nil
+}
+
+func buildLogNameAliasEntries(names []string, minPrefixLen int) ([]logNameAliasEntry, error) {
+	if minPrefixLen < 1 {
+		minPrefixLen = 1
+	}
+
+	entries := make([]logNameAliasEntry, len(names))
+	keyLens := make([]int, len(names))
+	for i, name := range names {
+		fullHash := logNameHashHex(name)
+		if minPrefixLen > len(fullHash) {
+			keyLens[i] = len(fullHash)
+		} else {
+			keyLens[i] = minPrefixLen
+		}
+		entries[i] = logNameAliasEntry{
+			Name:     name,
+			FullHash: fullHash,
+		}
+	}
+
+	for {
+		counts := map[string]int{}
+		for i, entry := range entries {
+			counts[entry.FullHash[:keyLens[i]]]++
+		}
+
+		changed := false
+		for i, entry := range entries {
+			key := entry.FullHash[:keyLens[i]]
+			if counts[key] <= 1 {
+				continue
+			}
+			if keyLens[i] >= len(entry.FullHash) {
+				return nil, errors.New("日志 hash 键冲突，请改用原始名称")
+			}
+			keyLens[i]++
+			changed = true
+		}
+
+		if !changed {
+			break
+		}
+	}
+
+	for i, entry := range entries {
+		entry.Key = entry.FullHash[:keyLens[i]]
+		entries[i] = entry
+	}
+	return entries, nil
+}
+
+func logNameHashHex(name string) string {
+	return fmt.Sprintf("%016x", xxhash.Sum64String(name))
+}
+
+func (index *logNameAliasIndex) Resolve(input string) (string, bool) {
+	if input == "" {
+		return "", false
+	}
+	if name, exists := index.byName[input]; exists {
+		return name, true
+	}
+	if name, exists := index.byKey[strings.ToLower(input)]; exists {
+		return name, true
+	}
+	return "", false
+}
+
+func resolveLogNameForGroup(operator engine.DatabaseOperator, groupID, input string) (string, error) {
+	if input == "" {
+		return "", nil
+	}
+	index, err := buildLogNameAliasIndex(operator, groupID)
+	if err != nil {
+		return "", err
+	}
+	if resolved, ok := index.Resolve(input); ok {
+		return resolved, nil
+	}
+	return input, nil
+}
+
+func formatLogNameForDisplay(name string) string {
+	if strings.TrimSpace(name) != name {
+		return strconv.Quote(name)
+	}
+	for _, r := range name {
+		if unicode.IsControl(r) {
+			return strconv.Quote(name)
+		}
+	}
+	return name
+}
+
+func formatLogNameListLine(entry logNameAliasEntry) string {
+	return fmt.Sprintf("- 【%s】: %s", entry.Key, formatLogNameForDisplay(entry.Name))
+}

dice/ext_log_alias_test.go
@@ -1,0 +1,221 @@
+//nolint:testpackage
+package dice
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"testing"
+
+	sealdiceLogger "Scardice-core/logger"
+	"Scardice-core/model"
+	"Scardice-core/utils/constant"
+)
+
+func newLogAliasTestDB(t *testing.T) *mockDatabaseOperator {
+	t.Helper()
+
+	mockDB, err := newMockDatabaseOperator(filepath.Join(t.TempDir(), "logs.db"))
+	if err != nil {
+		t.Fatalf("newMockDatabaseOperator: %v", err)
+	}
+	t.Cleanup(mockDB.Close)
+
+	db := mockDB.GetLogDB(constant.WRITE)
+	if err := db.AutoMigrate(&model.LogInfo{}, &model.LogOneItem{}); err != nil {
+		t.Fatalf("AutoMigrate: %v", err)
+	}
+	return mockDB
+}
+
+func appendTestLog(t *testing.T, mockDB *mockDatabaseOperator, groupID, logName string) {
+	t.Helper()
+
+	ok := LogAppend(&MsgContext{Dice: &Dice{DBOperator: mockDB}}, groupID, logName, &model.LogOneItem{
+		Nickname: "tester",
+		IMUserID: "user",
+		Message:  "line",
+	})
+	if !ok {
+		t.Fatalf("LogAppend failed for %q", logName)
+	}
+}
+
+func TestBuildLogNameAliasIndexUsesWrappedKeysAndResolvesInput(t *testing.T) {
+	mockDB := newLogAliasTestDB(t)
+	groupID := "QQ-Group:1001"
+
+	appendTestLog(t, mockDB, groupID, "normal-log")
+	appendTestLog(t, mockDB, groupID, " spaced\nname ")
+
+	index, err := buildLogNameAliasIndex(mockDB, groupID)
+	if err != nil {
+		t.Fatalf("buildLogNameAliasIndex: %v", err)
+	}
+	if len(index.entries) != 2 {
+		t.Fatalf("len(entries) = %d, want 2", len(index.entries))
+	}
+
+	for _, entry := range index.entries {
+		if len(entry.Key) < 8 {
+			t.Fatalf("key %q shorter than 8 chars", entry.Key)
+		}
+		if entry.Name == "normal-log" {
+			if got, ok := index.Resolve(entry.Key); !ok || got != entry.Name {
+				t.Fatalf("Resolve(%q) = (%q, %v), want (%q, true)", entry.Key, got, ok, entry.Name)
+			}
+		}
+	}
+
+	if got := formatLogNameForDisplay(" spaced\nname "); got != strconv.Quote(" spaced\nname ") {
+		t.Fatalf("formatLogNameForDisplay() = %q", got)
+	}
+	if got := formatLogNameListLine(logNameAliasEntry{Key: "deadbeef", Name: "normal-log"}); got != "- 【deadbeef】: normal-log" {
+		t.Fatalf("formatLogNameListLine() = %q", got)
+	}
+}
+
+func TestBuildLogNameAliasEntriesExtendsConflictingPrefixes(t *testing.T) {
+	buckets := map[byte][]string{}
+	for i := range 4096 {
+		name := fmt.Sprintf("log-%d", i)
+		hash := logNameHashHex(name)
+		prefix := hash[0]
+		buckets[prefix] = append(buckets[prefix], name)
+		if len(buckets[prefix]) >= 2 {
+			break
+		}
+	}
+
+	var names []string
+	for _, candidates := range buckets {
+		if len(candidates) >= 2 {
+			names = candidates[:2]
+			break
+		}
+	}
+	if len(names) != 2 {
+		t.Fatal("failed to generate colliding 1-char hash prefixes")
+	}
+
+	entries, err := buildLogNameAliasEntries(names, 1)
+	if err != nil {
+		t.Fatalf("buildLogNameAliasEntries: %v", err)
+	}
+
+	keys := map[string]struct{}{}
+	grew := false
+	for _, entry := range entries {
+		if _, exists := keys[entry.Key]; exists {
+			t.Fatalf("duplicate key %q", entry.Key)
+		}
+		keys[entry.Key] = struct{}{}
+		if len(entry.Key) > 1 {
+			grew = true
+		}
+	}
+	if !grew {
+		t.Fatal("expected at least one conflicting prefix to grow beyond min length")
+	}
+}
+
+func TestResolveLogNameForGroupPrefersExactNameBeforeAlias(t *testing.T) {
+	mockDB := newLogAliasTestDB(t)
+	groupID := "QQ-Group:1002"
+
+	appendTestLog(t, mockDB, groupID, "abcdef12")
+	appendTestLog(t, mockDB, groupID, "other-log")
+
+	index, err := buildLogNameAliasIndex(mockDB, groupID)
+	if err != nil {
+		t.Fatalf("buildLogNameAliasIndex: %v", err)
+	}
+
+	var alias string
+	for _, entry := range index.entries {
+		if entry.Name == "other-log" {
+			alias = entry.Key
+			break
+		}
+	}
+	if alias == "" {
+		t.Fatal("expected alias for other-log")
+	}
+	if strings.EqualFold(alias, "abcdef12") {
+		t.Fatal("test setup produced ambiguous exact-name collision")
+	}
+
+	got, err := resolveLogNameForGroup(mockDB, groupID, "abcdef12")
+	if err != nil {
+		t.Fatalf("resolveLogNameForGroup exact: %v", err)
+	}
+	if got != "abcdef12" {
+		t.Fatalf("resolve exact = %q, want %q", got, "abcdef12")
+	}
+}
+
+func TestLogSendToBackendAcceptsAliasKey(t *testing.T) {
+	mockDB := newLogAliasTestDB(t)
+	groupID := "QQ-Group:1003"
+	logName := "special name"
+	appendTestLog(t, mockDB, groupID, logName)
+
+	index, err := buildLogNameAliasIndex(mockDB, groupID)
+	if err != nil {
+		t.Fatalf("buildLogNameAliasIndex: %v", err)
+	}
+	var alias string
+	for _, entry := range index.entries {
+		if entry.Name == logName {
+			alias = entry.Key
+			break
+		}
+	}
+	if alias == "" {
+		t.Fatal("expected alias for special name")
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/dice/api/log" {
+			t.Fatalf("unexpected path %q", r.URL.Path)
+		}
+		if parseErr := r.ParseMultipartForm(1 << 20); parseErr != nil {
+			t.Fatalf("ParseMultipartForm: %v", parseErr)
+		}
+		if got := r.FormValue("name"); got != logName {
+			t.Fatalf("uploaded name = %q, want %q", got, logName)
+		}
+		_, _ = w.Write([]byte(`{"url":"https://example.com/log"}`))
+	}))
+	defer server.Close()
+
+	oldBackends := BackendUrls
+	BackendUrls = []string{server.URL}
+	t.Cleanup(func() { BackendUrls = oldBackends })
+
+	ctx := &MsgContext{
+		Dice: &Dice{
+			BaseConfig: BaseConfig{DataDir: t.TempDir()},
+			DBOperator: mockDB,
+			Logger:     sealdiceLogger.M(),
+		},
+		EndPoint: &EndPointInfo{EndPointInfoBase: EndPointInfoBase{UserID: "UI:1000"}},
+	}
+
+	unofficial, url, notice, err := LogSendToBackend(ctx, groupID, alias)
+	if err != nil {
+		t.Fatalf("LogSendToBackend: %v", err)
+	}
+	if unofficial {
+		t.Fatal("expected official backend path")
+	}
+	if notice != "" {
+		t.Fatalf("unexpected notice %q", notice)
+	}
+	if url != "https://example.com/log" {
+		t.Fatalf("url = %q", url)
+	}
+}

dice/storylog/upload_v105.go
@@ -21,7 +21,6 @@ import (
 
 	"Scardice-core/dice/service"
 	"Scardice-core/model"
-	"Scardice-core/utils"
 )
 
 func GetLogTxtAndParquetFile(env UploadEnv) (*os.File, *bytes.Buffer, error) {
@@ -34,10 +33,8 @@ func GetLogTxtAndParquetFile(env UploadEnv) (*os.File, *bytes.Buffer, error) {
 			),
 		))
 	buf := new(bytes.Buffer)
-	tempLog, err := os.CreateTemp("", fmt.Sprintf(
-		"%s(*).txt",
-		utils.FilenameClean("Scardice_v105_prefix_"),
-	))
+	tempPattern, _ := buildTempPattern("Scardice_v105_prefix")
+	tempLog, err := os.CreateTemp("", tempPattern)
 	if err != nil {
 		return nil, nil, errors.New("log导出出现未知错误")
 	}
@@ -76,12 +73,16 @@ func GetLogTxtAndParquetFile(env UploadEnv) (*os.File, *bytes.Buffer, error) {
 				for _, line := range cursorLines {
 					timeTxt := time.Unix(line.Time, 0).Format("2006-01-02 15:04:05")
 					text := fmt.Sprintf("%s(%v) %s\n%s\n\n", line.Nickname, line.IMUserID, timeTxt, line.Message)
-					_, _ = tempLog.WriteString(text)
+					if _, err0 = tempLog.WriteString(text); err0 != nil {
+						resultCh <- fmt.Errorf("写入日志导出临时文件失败: %w", err0)
+						return
+					}
 					counter++
 				}
 				// ========== 新增：每批写入后强制同步 ==========
 				if err = tempLog.Sync(); err != nil { // 确保批次数据落盘
 					resultCh <- fmt.Errorf("批次同步失败: %w", err)
+					return
 				}
 
 				_, err0 = parquetBuffer.Write(cursorLines)
@@ -132,7 +133,7 @@ func GetLogTxtAndParquetFile(env UploadEnv) (*os.File, *bytes.Buffer, error) {
 }
 
 // 一个不那么激进的改版
-func uploadV105(env UploadEnv) (string, error) {
+func uploadV105(env UploadEnv) (string, string, error) {
 	_ = os.MkdirAll(env.Dir, 0o755)
 
 	url, uploadTS, updateTS, _ := service.LogGetUploadInfo(env.Db, env.GroupID, env.LogName)
@@ -145,7 +146,7 @@ func uploadV105(env UploadEnv) (string, error) {
 			time.Unix(updateTS, 0).Format("2006-01-02 15:04:05"),
 			url,
 		)
-		return url, nil
+		return url, env.Notice, nil
 	}
 	if len(url) == 0 {
 		env.Log.Infof("没有查询到之前上传的URL Log:%s.%s", env.GroupID, env.LogName)
@@ -160,11 +161,15 @@ func uploadV105(env UploadEnv) (string, error) {
 
 	file, b, err := GetLogTxtAndParquetFile(env)
 	if err != nil {
-		return "", err
+		return "", env.Notice, err
 	}
+	defer func() {
+		_ = file.Close()
+		_ = os.Remove(file.Name())
+	}()
 	err = formatAndBackupV105(&env, file, b)
 	if err != nil {
-		return "", err
+		return "", env.Notice, err
 	}
 
 	url = parquetUploadToSealBackends(env, b.Bytes())
@@ -172,31 +177,48 @@ func uploadV105(env UploadEnv) (string, error) {
 		env.Log.Errorf("记录Log上传信息失败: %v", errDB)
 	}
 	if len(url) == 0 {
-		return "", errors.New("上传 log 到服务器失败，未能获取染色器链接")
+		return "", env.Notice, errors.New("上传 log 到服务器失败，未能获取染色器链接")
 	}
-	return url, nil
+	return url, env.Notice, nil
 }
 
 // formatAndBackup 将导出的日志序列化到 env.data 并存储为本地 zip
-func formatAndBackupV105(env *UploadEnv, tempLog *os.File, parquetFile *bytes.Buffer) error {
-	fzip, _ := os.OpenFile(
-		filepath.Join(env.Dir, utils.FilenameClean(fmt.Sprintf(
-			"%s_%s.%s.zip",
-			env.GroupID, env.LogName, time.Now().Format("060102150405"),
-		))),
+func formatAndBackupV105(env *UploadEnv, tempLog *os.File, parquetFile *bytes.Buffer) (err error) {
+	filename, notice := buildLogBackupFilename(env.GroupID, env.LogName, time.Now())
+	env.appendNotice(notice)
+	fzip, err := os.OpenFile(
+		filepath.Join(env.Dir, filename),
 		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
 		0o600,
 	)
+	if err != nil {
+		return fmt.Errorf("创建日志备份文件失败: %w", err)
+	}
 	writer := zip.NewWriter(fzip)
-	var err error
+	defer func() {
+		if closeErr := writer.Close(); err == nil && closeErr != nil {
+			err = fmt.Errorf("关闭日志压缩文件失败: %w", closeErr)
+		}
+		if closeErr := fzip.Close(); err == nil && closeErr != nil {
+			err = fmt.Errorf("关闭日志备份文件失败: %w", closeErr)
+		}
+	}()
 	// 写入README文件
 	{
-		wr, _ := writer.Create(ExportReadmeFilename)
-		_, _ = wr.Write([]byte(ExportReadmeContent))
+		readmeWriter, createErr := writer.Create(ExportReadmeFilename)
+		if createErr != nil {
+			return fmt.Errorf("创建README失败: %w", createErr)
+		}
+		if _, writeErr := readmeWriter.Write([]byte(ExportReadmeContent)); writeErr != nil {
+			return fmt.Errorf("写入README失败: %w", writeErr)
+		}
 	}
 	// 写入文本TXT文件
 
-	fileWriter, _ := writer.Create(ExportTxtFilename)
+	fileWriter, err := writer.Create(ExportTxtFilename)
+	if err != nil {
+		return fmt.Errorf("创建文本日志文件失败: %w", err)
+	}
 	if _, err = io.Copy(fileWriter, tempLog); err != nil {
 		return fmt.Errorf("写入文本日志文件失败: %w", err)
 	}
@@ -209,11 +231,7 @@ func formatAndBackupV105(env *UploadEnv, tempLog *os.File, parquetFile *bytes.Bu
 	if _, err = parquetWriter.Write(parquetFile.Bytes()); err != nil {
 		return fmt.Errorf("写入Parquet文件失败: %w", err)
 	}
-
-	_ = writer.Close()
-	_ = fzip.Close()
-
-	return err
+	return nil
 }
 
 func parquetUploadToSealBackends(env UploadEnv, payload []byte) string {