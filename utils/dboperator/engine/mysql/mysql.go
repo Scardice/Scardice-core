sealdice/sealdice-core

fix(logger): 恢复原版本的logger以避免产生空trace记录 (#1706)

utils/dboperator/engine/mysql/mysql.go
@@ -3,8 +3,8 @@ package mysql
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
+	"sealdice-core/logger"
 	"sealdice-core/utils/cache"
 )
 
@@ -13,7 +13,7 @@ func MySQLDBInit(dsn string) (*gorm.DB, error) {
 	// 使用 GORM 连接 MySQL
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		// 注意，这里虽然是Info,但实际上打印就变成了Debug.
-		Logger: logger.Default.LogMode(logger.Info)})
+		Logger: logger.DefaultSealLogger})
 	if err != nil {
 		return nil, err
 	}

utils/dboperator/engine/pgsql/pgsql.go
@@ -3,18 +3,16 @@ package pgsql
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
+	"sealdice-core/logger"
 	"sealdice-core/utils/cache"
 )
 
 func PostgresDBInit(dsn string) (*gorm.DB, error) {
-	// 构建 PostgreSQL DSN (Data Source Name)
-
 	// 使用 GORM 连接 PostgreSQL
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		// 注意，这里虽然是Info,但实际上打印就变成了Debug.
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.DefaultSealLogger,
 	})
 	if err != nil {
 		return nil, err

utils/dboperator/engine/sqlite/sqlite.go
@@ -7,12 +7,11 @@ import (
 	"fmt"
 	"runtime"
 
-	_ "github.com/ncruces/go-sqlite3/embed"
-	sqlite "github.com/ncruces/go-sqlite3/gormlite"
+	"github.com/glebarez/sqlite"
 
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
+	"sealdice-core/logger"
 	"sealdice-core/utils/cache"
 )
 
@@ -22,8 +21,7 @@ func SQLiteDBInit(path string, useWAL bool) (*gorm.DB, error) {
 	// 使用即时事务
 	path = fmt.Sprintf("file:%v?_txlock=immediate&_busy_timeout=15000", path)
 	open, err := gorm.Open(sqlite.Open(path), &gorm.Config{
-		// 注意，这里虽然是Info,但实际上打印就变成了Debug.
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.DefaultSealLogger,
 	})
 	if err != nil {
 		return nil, err
@@ -85,7 +83,7 @@ func createWriteDB(path string, gormConf gorm.Config) (*gorm.DB, error) {
 func SQLiteDBRWInit(path string) (*gorm.DB, *gorm.DB, error) {
 	// 由于现在我们只有一个写入连接，所以不需要使用事务
 	gormConf := gorm.Config{
-		Logger:                 logger.Default.LogMode(logger.Info),
+		Logger:                 logger.DefaultSealLogger,
 		SkipDefaultTransaction: true,
 	}
 	readDB, err := createReadDB(path, gormConf)

utils/dboperator/engine/sqlite/sqlite_cgo.go
@@ -9,8 +9,8 @@ import (
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
+	"sealdice-core/logger"
 	"sealdice-core/utils/cache"
 )
 
@@ -20,8 +20,7 @@ func SQLiteDBInit(path string, useWAL bool) (*gorm.DB, error) {
 	// 使用即时事务
 	path = fmt.Sprintf("file:%v?_txlock=immediate&_busy_timeout=15000", path)
 	open, err := gorm.Open(sqlite.Open(path), &gorm.Config{
-		// 注意，这里虽然是Info,但实际上打印就变成了Debug.
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.DefaultSealLogger,
 	})
 	if err != nil {
 		return nil, err
@@ -82,7 +81,7 @@ func createWriteDB(path string, gormConf gorm.Config) (*gorm.DB, error) {
 func SQLiteDBRWInit(path string) (*gorm.DB, *gorm.DB, error) {
 	// 由于现在我们只有一个写入连接，所以不需要使用事务
 	gormConf := gorm.Config{
-		Logger:                 logger.Default.LogMode(logger.Info),
+		Logger:                 logger.DefaultSealLogger,
 		SkipDefaultTransaction: true,
 	}
 	readDB, err := createReadDB(path, gormConf)

Scardice/Scardice-core

(cherry picked from commit 3b54716addfeece0a8fffbbe59b23aa424d17a1d)

utils/dboperator/engine/mysql/mysql.go
@@ -3,8 +3,8 @@ package mysql
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
+	"Scardice-core/logger"
 	"Scardice-core/utils/cache"
 )
 
@@ -13,7 +13,7 @@ func MySQLDBInit(dsn string) (*gorm.DB, error) {
 	// 使用 GORM 连接 MySQL
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		// 注意，这里虽然是Info,但实际上打印就变成了Debug.
-		Logger: logger.Default.LogMode(logger.Info)})
+		Logger: logger.DefaultSealLogger})
 	if err != nil {
 		return nil, err
 	}

utils/dboperator/engine/pgsql/pgsql.go
@@ -3,18 +3,16 @@ package pgsql
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
+	"Scardice-core/logger"
 	"Scardice-core/utils/cache"
 )
 
 func PostgresDBInit(dsn string) (*gorm.DB, error) {
-	// 构建 PostgreSQL DSN (Data Source Name)
-
 	// 使用 GORM 连接 PostgreSQL
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		// 注意，这里虽然是Info,但实际上打印就变成了Debug.
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.DefaultSealLogger,
 	})
 	if err != nil {
 		return nil, err

utils/dboperator/engine/sqlite/sqlite.go
@@ -7,12 +7,11 @@ import (
 	"fmt"
 	"runtime"
 
-	_ "github.com/ncruces/go-sqlite3/embed"
-	sqlite "github.com/ncruces/go-sqlite3/gormlite"
+	"github.com/glebarez/sqlite"
 
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
+	"Scardice-core/logger"
 	"Scardice-core/utils/cache"
 )
 
@@ -22,8 +21,7 @@ func SQLiteDBInit(path string, useWAL bool) (*gorm.DB, error) {
 	// 使用即时事务
 	path = fmt.Sprintf("file:%v?_txlock=immediate&_busy_timeout=15000", path)
 	open, err := gorm.Open(sqlite.Open(path), &gorm.Config{
-		// 注意，这里虽然是Info,但实际上打印就变成了Debug.
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.DefaultSealLogger,
 	})
 	if err != nil {
 		return nil, err
@@ -85,7 +83,7 @@ func createWriteDB(path string, gormConf gorm.Config) (*gorm.DB, error) {
 func SQLiteDBRWInit(path string) (*gorm.DB, *gorm.DB, error) {
 	// 由于现在我们只有一个写入连接，所以不需要使用事务
 	gormConf := gorm.Config{
-		Logger:                 logger.Default.LogMode(logger.Info),
+		Logger:                 logger.DefaultSealLogger,
 		SkipDefaultTransaction: true,
 	}
 	readDB, err := createReadDB(path, gormConf)

utils/dboperator/engine/sqlite/sqlite_cgo.go
@@ -9,8 +9,8 @@ import (
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
+	"Scardice-core/logger"
 	"Scardice-core/utils/cache"
 )
 
@@ -20,8 +20,7 @@ func SQLiteDBInit(path string, useWAL bool) (*gorm.DB, error) {
 	// 使用即时事务
 	path = fmt.Sprintf("file:%v?_txlock=immediate&_busy_timeout=15000", path)
 	open, err := gorm.Open(sqlite.Open(path), &gorm.Config{
-		// 注意，这里虽然是Info,但实际上打印就变成了Debug.
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.DefaultSealLogger,
 	})
 	if err != nil {
 		return nil, err
@@ -82,7 +81,7 @@ func createWriteDB(path string, gormConf gorm.Config) (*gorm.DB, error) {
 func SQLiteDBRWInit(path string) (*gorm.DB, *gorm.DB, error) {
 	// 由于现在我们只有一个写入连接，所以不需要使用事务
 	gormConf := gorm.Config{
-		Logger:                 logger.Default.LogMode(logger.Info),
+		Logger:                 logger.DefaultSealLogger,
 		SkipDefaultTransaction: true,
 	}
 	readDB, err := createReadDB(path, gormConf)